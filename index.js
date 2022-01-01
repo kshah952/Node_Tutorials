718f9a5436c1b5c67190cf86b9d77f392ae1f81d
kshah952 <kshah952@rku.ac.in>
2022-01-01 13:06:11 +0530

    Hands on Node 6 Query Completed

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="p6.js">
+        Name:<input type="text" value="" name="fname"><br>
+        Lname:<input type="text" value="" name="lname"><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
